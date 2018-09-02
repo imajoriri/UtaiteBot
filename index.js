87ffbe36fa51eb4905f44001238d8236b3f3fc3e
imajoriri <imajo.srroni@gmail.com>
2018-09-02 13:35:17 +0900

    全体のエラー処理

--- index.js
+++ index.js
@@ -17,27 +17,36 @@ exports.handler = async function(event) {
   let response = { statusCode: 200 };
   var replyMessage = [];
 
-  // メッセージがテキストだった時
-  if(event.events[0].message.type === "text"){
-    var requestMsg = event.events[0].message.text;
+  try{
+    // メッセージがテキストだった時
+    if(event.events[0].message.type === "text"){
+      var requestMsg = event.events[0].message.text;
 
-    // TODO
-    // userId >> events[0].source.userId 多分
+      if(requestMsg === "1"){
+        
+        replyMessage = await getLatestSongs(event);
+        console.log(replyMessage);
 
-    if(requestMsg === "1"){
-      replyMessage = await getLatestSongs(event);
-      console.log(replyMessage);
-    } else if(requestMsg === "2"){
-      // ランダムで１曲取得
-      replyMessage = await getRandomSongs(event, 1);
-    } else if(requestMsg === "3"){
-    } else if(requestMsg === "4"){
-      // アプリの使い方を説明
-      replyMessage = await getHowToUse(event);
+      } else if(requestMsg === "2"){
+
+        // ランダムで１曲取得
+        replyMessage = await getRandomSongs(event, 1);
+
+      } else if(requestMsg === "3"){
+
+      } else if(requestMsg === "4"){
+
+        // アプリの使い方を説明
+        replyMessage = await getHowToUse(event);
+
+      }
+
+    } else {
+      replyMessage.push({ 'type': 'text', 'text': "テキストメッセージを入力してください。" });
     }
-
-  } else {
-    replyMessage.push({ 'type': 'text', 'text': "テキストメッセージを入力してください。" });
+  }catch(e){
+    replyMessage.push({ 'type': 'text', 'text': "申し訳ございません。エラーが発生しました。" });
+    console.log(e)
   }
 
   // Botにメッセージをリプライ
